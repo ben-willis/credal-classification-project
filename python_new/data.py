8ccf5a19bfc184351f5e564cd459fb551470d671
Ben Willis <benwillis0612@gmail.com>
2017-02-05 21:10:26 +0000

    New python maximum liklihood classifier

---
+++ python_new/data.py
@@ -0,0 +1,31 @@
+import os, csv
+
+def import_data(path):
+	data = []
+	with open(os.path.join(os.path.dirname(__file__), path)) as datafile:
+		next(datafile, None)
+		for row in csv.reader(datafile):
+			data.append([int(value) for value in row])
+	return data
+
+def transpose(M):
+	return [list(i) for i in zip(*M)]
+
+def remove_row(M, j):
+	return M[:j] + M[j+1:]
+
+def remove_column(M, i):
+	M_t = transpose(M)
+	M_t = remove_row(M_t, i)
+	return transpose(M_t)
+
+def remove_rows_with_missing_values(M):
+	for j in range(len(M)-1, -1, -1):
+		if -1 in M[j]:
+			M = remove_row(M, j)
+	return M
+
+data = import_data('../data/trees_discretized.csv')
+data = remove_column(data, 0)
+data = remove_rows_with_missing_values(data)
+
