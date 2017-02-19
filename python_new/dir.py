7072bed51c8c8233093e067d017f903708810707
Ben Willis <benwillis0612@gmail.com>
2017-02-19 17:05:10 +0000

    Code to play around with loss functions

--- python_new/dir.py
+++ python_new/dir.py
@@ -2,7 +2,8 @@ from __future__ import division
 import random, math
 
 from data import data
-from diagnostics import accuracy, mse, cross_validate
+from diagnostics import accuracy, mse, cross_validate, compare
+from lossfns import zero_one, squared_diff,  absolute_diff
 
 def transpose(M):
 	return [list(i) for i in zip(*M)]
@@ -36,16 +37,21 @@ def train_classifier(data, values, a_ids, c_id):
 			p_c_given_ais[c] = p_cs[c]
 			for a_id in a_ids:
 				p_c_given_ais[c] = p_c_given_ais[c] * p_ai_given_cs[a_id][c][obj[a_id]]
-		return p_c_given_ais.index(max(p_c_given_ais))
+		normalizing_factor = sum(p_c_given_ais)
+		p_c_given_ais = [prob/normalizing_factor for prob in p_c_given_ais]
+		return (zero_one(p_c_given_ais), squared_diff(p_c_given_ais), absolute_diff(p_c_given_ais))
 	return trained_classifier
 
-res = []
-se = 1000
-res.append(cross_validate(data, 24, train_classifier, mse, 5))
-while(se/math.sqrt(len(res)) > 0.002):
-	res.append(cross_validate(data, 24, train_classifier, mse, 5))
-	mean = sum(res)/len(res)
-	se = math.sqrt( (1/(len(res)-1)) * sum([(x - mean)**2 for x in res]) )
-	print mean
-print(len(res))
-print mean
+# res = []
+# se = 1000
+# res.append(cross_validate(data, 24, train_classifier, mse, 10))
+# while(se/math.sqrt(len(res)) > 0.005):
+# 	res.append(cross_validate(data, 24, train_classifier, mse, 10))
+# 	mean = sum(res)/len(res)
+# 	se = math.sqrt( (1/(len(res)-1)) * sum([(x - mean)**2 for x in res]) )
+# print mean
+
+test = cross_validate(data, 24, train_classifier, compare, 10)
+
+for row in test:
+	print row
